aac3119051f66d4c485c4770f11a313a2944d4c1
Sandeep <sandipsharma2445@gmail.com>
2025-09-04 10:09:47 +0530

    cors configured

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const connectDB = require("./db");
 const authRouter = require("./routes/auth.routes");
 const userRouter = require("./routes/user.routes");
 const chatsRouter = require("./routes/chats.routes");
 const messageRouter = require("./routes/message.routes");
+const { allowedOrigins } = require("./utils/allowedOrigins");
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 
 app.use(express.json()); 
 
